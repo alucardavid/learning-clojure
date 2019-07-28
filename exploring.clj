0109cec9c6fecc9fb771ed05691df429ea1246de
David Pereira <d_pereira@outlook.com.br>
2019-07-28 15:04:03 -0300

    Functions pt1

--- exploring.clj
+++ exploring.clj
@@ -1,6 +1,23 @@
-(defn greeting
+
+; Simple function
+(defn greeting1
     "Returns a greeting of the form 'Hello, username.'"
     [username]
     (str "Hello, " username))
 
-(greeting "David")
+; Function with zero-argument
+(defn greeting2
+    "Returns a greeting of the form 'Hello, username.'
+    Default username is 'world'."
+    ([] (greeting "world"))
+    ([username] (str "Hello, " username)))
+
+; Function with a sequence of parameters
+(defn date [person-1 person-2 & chaperones]
+    (println person-1 " and " person-2
+        " went out with " (count chaperones) " chaperones."))
+
+; Anonymous function
+(require '[clojure.string :as str])
+(filter (fn [w] (> (count w) 2)) (str/split "A fine day it is" #"\W+"))
+
